b57f51fb1d1c3beeb9c76eeadf747737f0a8e39c
孙兴松 <sunxingsong@sunxingongdembp.avaya.com>
2023-03-22 09:18:48 +0800

    fixed end element issue

---
+++ src/test.rs
@@ -0,0 +1,44 @@
+#[cfg(test)]
+mod tests {
+    use crate::{
+        ds::{
+            CanonicalizationMethod, DigestMethod, Reference, SignatureMethod, SignedInfo,
+            Transform, Transforms,
+        },
+        to_events,
+    };
+
+    #[test]
+    fn to_event() {
+        let sign_info = SignedInfo {
+            canonicalization_method: CanonicalizationMethod {
+                algorithm: "http://www.w3.org/TR/2001/REC-xml-c14n-20010315".to_string(),
+            },
+            signature_method: SignatureMethod {
+                algorithm: "http://www.w3.org/2000/09/xmldsig#rsa-sha1".to_string(),
+            },
+            reference: [Reference {
+                transforms: Some(Transforms {
+                    transforms: [Transform {
+                        algorithm: "http://www.w3.org/2000/09/xmldsig#enveloped-signature"
+                            .to_string(),
+                    }]
+                    .to_vec(),
+                }),
+                digest_method: DigestMethod {
+                    algorithm: "http://www.w3.org/2000/09/xmldsig#sha1".to_string(),
+                },
+                digest_value: "iEauNqezvNrpcl8YJBj9OhziUXQ=".to_string(),
+                id: None,
+                uri: Some("".to_string()),
+                ref_type: None,
+            }]
+            .to_vec(),
+            id: None,
+        };
+
+        let result = to_events(&sign_info).unwrap();
+
+        println!("result : {result:?}");
+    }
+}
